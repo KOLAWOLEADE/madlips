1c1624e80b26ed879e13c8eabfa45c15a2e82011
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-05-28 14:29:12 +0100

    add function madlibs

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Madlibs function
+
+let verb = (prompt("type in your verb"));
+let noun = (prompt("type your noun "));
+let verb2 = (prompt("type in your verb2"))
+
+
+
+function madlibs(){
+
+    let story = " I " + verb + " " + " up in " + noun+ ", at aged six, i was registered for a leaving school certificate. After six years i " +verb2+ "  to nearby secondary school for my junior and senior school certificate." 
+
+    return story;
+}
+
+alert (madlibs());
+console.log (madlibs());
+document.write ( madlibs());
